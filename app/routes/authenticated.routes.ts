b4b820502ee53fa3629ced9590e54ca9be3402fa
Marley Martins <marley.martins@interestengenharia.com.br>
2026-09-22 10:36:43 -0300

    refactor(empresa): remove EmpresaController and related routes and services

--- app/routes/authenticated.routes.ts
+++ app/routes/authenticated.routes.ts
@@ -1,7 +1,6 @@
 import { FastifyInstance } from "fastify";
 import authMiddleware from "../middleware/middleware";
 import userRoutes from "./user.routes";
-import empresaRoutes from "./empresa.routes";
 import { clientRoutes } from "./client.routes";
 import { carRoutes } from "./car.route";
 import { servicoRoutes } from "./servico.routes";
@@ -9,7 +8,6 @@ import { servicoRoutes } from "./servico.routes";
 const authenticatedRoutes = (app: FastifyInstance) => {
     app.addHook("onRequest", authMiddleware);
     app.register(userRoutes);
-    app.register(empresaRoutes);
     app.register(clientRoutes);
     app.register(servicoRoutes);
     app.register(carRoutes);
--- app/routes/empresa.routes.ts
+++
@@ -1,9 +0,0 @@
-import { FastifyInstance } from "fastify";
-import { EmpresaController } from "../controller/empresaController";
-
-const empresaRoutes = async (app: FastifyInstance) => {
-  app.get("/empresas", EmpresaController.getEmpresas);
-  app.get("/empresas/me", EmpresaController.getEmpresaUser);
-};
-
-export default empresaRoutes;
